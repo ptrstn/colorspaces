a68051a134f80074fc693e96c47c9eedee27bfb0
Peter Stein <peterstein@protonmail.com>
2020-05-09 23:30:56 +0200

    Add version number to setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ from setuptools import setup
 
 setup(
     name="colorspaces",
+    version="0.0.1",
+    url="http://github.com/ptrstn/colorspaces",
+    author="Peter Stein",
+    license="WTFPL",
     packages=["colorspaces"],
     install_requires=["numpy", "matplotlib", "Pillow", "scikit-image"],
 )
