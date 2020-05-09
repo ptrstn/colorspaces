2304d5ef1ec5bd4261a398e8c3bd4b527d5594b4
Peter Stein <peterstein@protonmail.com>
2020-05-09 23:48:11 +0200

    Refactor incorrect variable names

--- colorspaces/core.py
+++ colorspaces/core.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot
 from skimage.color import lab2rgb, rgb2lab
 
 
-def create_image_by_lab(lightness, a, b):
+def create_image_by_lab(lightness, a, b) -> Image:
     """
     Create a 500x500 RGB image by L*a*b* or CIELAB color space values
 
@@ -14,23 +14,23 @@ def create_image_by_lab(lightness, a, b):
     :param b: b* color value where -128 is blue and +128 is yellow
     :return: Image
     """
-    image = numpy.zeros((500, 500, 3))
-    image[:, :, 0] = lightness
-    image[:, :, 1] = a
-    image[:, :, 2] = b
+    lab_array = numpy.zeros((500, 500, 3))
+    lab_array[:, :, 0] = lightness
+    lab_array[:, :, 1] = a
+    lab_array[:, :, 2] = b
 
-    image_rgb_float = lab2rgb(image)
-    image_rgb_uint8 = skimage.img_as_ubyte(image_rgb_float)
+    rgb_array_float = lab2rgb(lab_array)
+    rgb_array_uint8 = skimage.img_as_ubyte(rgb_array_float)
 
-    return Image.fromarray(image_rgb_uint8, mode="RGB")
+    return Image.fromarray(rgb_array_uint8, mode="RGB")
 
 
-def create_image_by_rgb(red, green, blue):
-    rgb_pixel = numpy.zeros((500, 500, 3), dtype=numpy.uint8)
-    rgb_pixel[:, :, 0] = red
-    rgb_pixel[:, :, 1] = green
-    rgb_pixel[:, :, 2] = blue
-    return Image.fromarray(rgb_pixel, mode="RGB")
+def create_image_by_rgb(red, green, blue) -> Image:
+    rgb_array = numpy.zeros((500, 500, 3), dtype=numpy.uint8)
+    rgb_array[:, :, 0] = red
+    rgb_array[:, :, 1] = green
+    rgb_array[:, :, 2] = blue
+    return Image.fromarray(rgb_array, mode="RGB")
 
 
 def convert_lab_array_to_rgb(array: numpy.ndarray) -> numpy.ndarray:
