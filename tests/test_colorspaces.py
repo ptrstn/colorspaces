c9679ca18c4d873b544c397b361ff11eff9ad712
Peter Stein <peterstein@protonmail.com>
2020-05-10 23:47:25 +0200

    Add lab2xyz "UserWarning: Color data out of range: Z < 0" example
    Sometimes during conversion from L*a*b* to xyz color space a user warning is raised if the values are invalid.
    This commit adds an example (L*a*b* values [0, -13.70470524, 27.88690567]) of when this happens.
    
    "UserWarning: Color data out of range: Z < 0 in 1 pixels"
    
    This subject was discussed in https://github.com/scikit-image/scikit-image/issues/4506

--- tests/test_colorspaces.py
+++ tests/test_colorspaces.py
@@ -1,5 +1,8 @@
-import numpy
+import math
 
+import numpy
+import pytest
+from skimage.color import lab2xyz
 
 from colorspaces import (
     create_image_by_rgb,
@@ -22,14 +25,35 @@ def test_create_image_by_lab():
     assert numpy.array_equal(image_array[0, 0, :], numpy.array([254, 0, 0]))
 
 
-def test_convert_lab_array_to_rgb():
+def test_convert_rgb_array_to_lab():
     rgb_array = numpy.array([[[255, 0, 0]]], dtype="uint8")
     lab_array = convert_rgb_array_to_lab(rgb_array)
     expected_array = numpy.array([[[53.24058794, 80.09230823, 67.20275104]]])
     assert numpy.allclose(lab_array, expected_array)
 
 
-def test_convert_rgb_array_to_lab():
+def test_lab2xyz_user_warning():
+    """
+    Show how the xyz color space is not a complete superset of all possible L*a*b combinations.
+    z should always be greater than zero but are not in this example.
+
+    See: https://github.com/scikit-image/scikit-image/issues/4506
+    """
+    with pytest.warns(UserWarning):
+        xyz_array = lab2xyz(numpy.array([[[0, -13.70470524, 27.88690567]]]))
+
+    x, y, z = xyz_array[0, 0, :]
+
+    expected_x = -0.00334555
+    expected_y = 0
+    expected_z = -0.01928643
+
+    assert math.isclose(x, expected_x, rel_tol=1e-06)
+    assert math.isclose(y, expected_y)
+    assert math.isclose(z, expected_z, rel_tol=1e-06)
+
+
+def test_convert_lab_array_to_rgb():
     lab_array = numpy.array([[[53.24058794, 80.09230823, 67.20275104]]])
     rgb_array = convert_lab_array_to_rgb(lab_array)
     expected_array = numpy.array([[[255, 0, 0]]], dtype="uint8")
